d117df5a5406adfd52952a368022b85a6bc923fc
kolaynurdan <kolaynurdan@gmail.com>
2021-10-13 00:33:42 +0300

    updated files

---
+++ BusinessLayer/Concrete/WriterManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class WriterManager : IWriterService
+    {
+        IWriterDal _writerdal;
+
+        public WriterManager(IWriterDal writerdal)
+        {
+            _writerdal = writerdal;
+        }
+
+        public void WriterAdd(Writer writer)
+        {
+            _writerdal.Insert(writer);
+        }
+    }
+}
