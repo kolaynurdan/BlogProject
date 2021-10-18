0b95c1d615bce976e1934e47ba7880ea1f7a57dd
kolaynurdan <kolaynurdan@gmail.com>
2021-10-18 22:10:30 +0300

    updated NewsLetter

---
+++ BusinessLayer/Concrete/NewsLetterManager.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class NewsLetterManager:INewsLetterService
+    {
+        INewsletterDal _newsletterDal;
+
+        public NewsLetterManager(INewsletterDal newsletterDal)
+        {
+            _newsletterDal = newsletterDal;
+        }
+        public void AddNewsLetter(NewsLetter newsLetter)
+        {
+            _newsletterDal.Insert(newsLetter);
+        }
+    }
+}
