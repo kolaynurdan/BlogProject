34751cc6c28b494772189bbf14ecfd4bd5bdca10
kolaynurdan <kolaynurdan@gmail.com>
2021-11-23 00:15:45 +0300

    updated Authorization

--- CoreDemo/Controllers/LoginController.cs
+++ CoreDemo/Controllers/LoginController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +12,26 @@ namespace CoreDemo.Controllers
 {
     public class LoginController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Index(Writer p)
+        {
+            Context c = new Context();
+            var datavalue = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
+            if (datavalue != null)
+            {
+                HttpContext.Session.SetString("username", p.WriterMail);
+                return RedirectToAction("Index", "Writer");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
