4a45d970deacbf3838e1a04bce3990af80a513e5
nurdan.kolay <kolaynurdan@gmail.com>
2022-01-22 19:51:38 +0300

    updated Writer Panel

--- CoreDemo/Controllers/LoginController.cs
+++ CoreDemo/Controllers/LoginController.cs
@@ -1,11 +1,13 @@
 ﻿using DataAccessLayer.Concrete;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace CoreDemo.Controllers
@@ -19,13 +21,19 @@ namespace CoreDemo.Controllers
         }
         [HttpPost]
         [AllowAnonymous]
-        public IActionResult Index(Writer p)
+        public async Task<IActionResult> Index(Writer p)
         {
             Context c = new Context();
-            var datavalue = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
-            if (datavalue != null)
+            var datavalue = c.Writers.FirstOrDefault(x => x.WriterMail==p.WriterMail && x.WriterPassword == p.WriterPassword);
+            if (datavalue !=null)
             {
-                HttpContext.Session.SetString("username", p.WriterMail);
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,p.WriterMail)
+                };
+                var useridentity = new ClaimsIdentity(claims,"a");
+                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+                await HttpContext.SignInAsync(principal);
                 return RedirectToAction("Index", "Writer");
             }
             else
@@ -35,3 +43,14 @@ namespace CoreDemo.Controllers
         }
     }
 }
+//Context c = new Context();
+//var datavalue = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
+//if (datavalue != null)
+//{
+//    HttpContext.Session.SetString("username", p.WriterMail);
+//    return RedirectToAction("Index", "Writer");
+//}
+//else
+//{
+//    return View();
+//}
--- CoreDemo/Startup.cs
+++ CoreDemo/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -26,12 +27,21 @@ namespace CoreDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddSession();
+            //services.AddSession();
             services.AddMvc(config =>
             {
                 var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
                 config.Filters.Add(new AuthorizeFilter(policy));
             });
+
+            services.AddMvc();
+            services.AddAuthentication(
+                CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(x =>
+                {
+                    x.LoginPath = "/Login/Index";
+                }
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,7 +61,9 @@ namespace CoreDemo
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            app.UseSession();
+            app.UseAuthentication();
+
+            //app.UseSession();
             app.UseRouting();
 
             app.UseAuthorization();
